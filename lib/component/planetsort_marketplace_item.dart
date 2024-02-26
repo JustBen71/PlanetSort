f774e17c8226c7f5d6b1ebb3bf1735a86ae7f1ff
MV <mathilde.vollet.auditeur@lecnam.net>
2024-02-26 12:19:27 +0800

    Improved Marketplace page

--- lib/component/planetsort_marketplace_item.dart
+++ lib/component/planetsort_marketplace_item.dart
@@ -2,52 +2,75 @@ import 'package:flutter/material.dart';
 
 import 'package:planetsort/utils/constant.dart';
 
+import 'package:url_launcher/url_launcher_string.dart';
 class PlanetSortMarketplaceItem extends StatelessWidget {
   const PlanetSortMarketplaceItem(
       {super.key,
       required this.imagePath,
       required this.nomArticle,
-      required this.price});
+      required this.price,
+      required this.url});
   final String imagePath;
   final String nomArticle;
   final String price;
+  final String url;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        color: green,
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Image.network(
-                  imagePath,
-                  height: 100,
-                  width: 100,
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Text(nomArticle,
+    return Expanded(
+      child:
+        TextButton(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: Image.network(
+                          imagePath,
+                          height: 100,
+                          width: 100,
+                        ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child:
+                      Text(nomArticle,
+                        style: const TextStyle(
+                            fontFamily: 'RockNRoll',
+                            color: beige,
+                            fontSize: 11,
+                        ),
+                        textAlign: TextAlign.center,
+                        softWrap: true,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      )
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(price,
                     style: const TextStyle(
-                        backgroundColor: green,
                         fontFamily: 'RockNRoll',
                         color: beige,
-                        fontSize: 11))
-              ],
-            ),
-            Row(
-              children: [
-                Text(price,
-                    style: const TextStyle(
-                        backgroundColor: green,
-                        fontFamily: 'RockNRoll',
-                        color: beige,
-                        fontSize: 11))
-              ],
-            ),
-          ],
-        ));
+                        fontSize: 11),
+                    textAlign: TextAlign.center)
+                ],
+              ),
+            ],
+          ),
+          onPressed: () {
+            launchUrlString(url);
+          }
+        )
+    );
   }
 }
--- lib/pages/marketplace_page.dart
+++ lib/pages/marketplace_page.dart
@@ -28,7 +28,7 @@ class MarketplacePage extends StatelessWidget {
               child: const Row(
                 children: [
                    PlanetSortText(
-                       data: "Here you will find our different partners and thier offers",
+                       data: "Here you will find our different partners and their offers.",
                        fontSize: paragraph,
                      )
                 ],
@@ -43,116 +43,127 @@ class MarketplacePage extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/41789770/r/il/f90d4d/5816837895/il_1588xN.5816837895_gdhz.jpg",
-                        nomArticle: "test",
-                        price: "30€",
+                      imagePath: "https://i.etsystatic.com/5317778/r/il/c34dde/4263377415/il_794xN.4263377415_ovba.jpg",
+                      nomArticle: "Organic paper towel",
+                      price: "SGD 25.98",
+                      url: 'https://www.etsy.com/sg-en/listing/257390846/serviettes-de-papier-en-coton-biologique'
+                    ),
+                    PlanetSortMarketplaceItem(
+                        imagePath: "https://i.etsystatic.com/19893040/r/il/0ddcd7/3907960016/il_794xN.3907960016_ej9x.jpg",
+                        nomArticle: "Recyclable pens",
+                        price: "SGD 10.84",
+                        url: 'https://www.etsy.com/sg-en/listing/1238880403/ensemble-de-stylos-ecologiques-paquet-de'
                       ),
                     PlanetSortMarketplaceItem(
-                      imagePath:
-                          "https://i.etsystatic.com/38996290/r/il/c4d35f/4649971740/il_1588xN.4649971740_70ez.jpg",
-                      nomArticle: "test2",
-                      price: "10€",
-                    ),
-                        
-                    
-                    PlanetSortMarketplaceItem(
-                      imagePath:
-                          "https://i.etsystatic.com/36754141/r/il/b2aa18/5750136829/il_1588xN.5750136829_dnip.jpg",
-                      nomArticle: "test3",
-                      price: "45€",
+                      imagePath: "https://i.etsystatic.com/10728526/r/il/43216e/3337167039/il_794xN.3337167039_issh.jpg",
+                      nomArticle: "Set of 2 notebooks",
+                      price: "SGD 15.53",
+                      url: 'https://www.etsy.com/sg-en/listing/1060391948/ensemble-de-2-carnets-carnets-oranges'
                     ),
                   ],
                 ),
-                Padding(padding: EdgeInsets.symmetric(vertical: 30.0)),
+                Padding(padding: EdgeInsets.symmetric(vertical: 20.0)),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/16740231/r/il/8b76ff/2084499904/il_1588xN.2084499904_8ywa.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/45655764/r/il/49d708/5521970707/il_794xN.5521970707_bpbv.jpg",
+                      nomArticle: "Reusable wooden cutlery set",
+                      price: "SGD 18.33",
+                      url: 'https://www.etsy.com/sg-en/listing/1536424023/ensemble-de-couverts-en-bois-de-style'
+                    ),
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/16429726/r/il/c56a73/5616041924/il_1588xN.5616041924_bt70.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/25827525/r/il/9baf09/5804013449/il_794xN.5804013449_3c3q.jpg",
+                      nomArticle: "Natural Coconut Milk Soap",
+                      price: "SGD 11.05",
+                      url: 'https://www.etsy.com/sg-en/listing/1674098779/savon-naturel-au-lait-de-coco-o-savon-au'
+                    ),
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/40472291/r/il/83f44d/5428681650/il_1588xN.5428681650_tc8c.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/12764689/r/il/c833e9/5563653482/il_794xN.5563653482_toxu.jpg",
+                      nomArticle: "Sustainable and eco-friendly kitchen kit",
+                      price: "SGD 80.22",
+                      url: 'https://www.etsy.com/sg-en/listing/1073907913/coffret-cadeau-kit-de-cuisine-durable'
+                    ),
                   ],
                 ),
-                Padding(padding: EdgeInsets.symmetric(vertical: 30.0)),
+                Padding(padding: EdgeInsets.symmetric(vertical: 20.0)),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/16740231/r/il/8b76ff/2084499904/il_1588xN.2084499904_8ywa.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/27920321/r/il/347332/5114683083/il_794xN.5114683083_5tez.jpg",
+                      nomArticle: "Reusable kitchen roll",
+                      price: "SGD 15.53",
+                      url: 'https://www.etsy.com/sg-en/listing/1283037690/rouleau-de-cuisine-reutilisable-chiffons'
+                    ),
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/16429726/r/il/c56a73/5616041924/il_1588xN.5616041924_bt70.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/18244890/r/il/ac8d72/5405032782/il_794xN.5405032782_c0qh.jpg",
+                      nomArticle: "Natural Beauty Hamper",
+                      price: "SGD 83.76",
+                      url: 'https://www.etsy.com/sg-en/listing/1322650751/panier-de-beaute-naturel-kit-de-soins'
+                    ),
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/40472291/r/il/83f44d/5428681650/il_1588xN.5428681650_tc8c.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/21078302/r/il/ba902f/3569206719/il_794xN.3569206719_9f4c.jpg",
+                      nomArticle: "Reusable Ear Buds",
+                      price: "SGD 11.85",
+                      url: 'https://www.etsy.com/sg-en/listing/1136330603/embout-q-reutilisable-en-bambou-avec'
+                    ),
                   ],
                 ),
-                Padding(padding: EdgeInsets.symmetric(vertical: 30.0)),
+                Padding(padding: EdgeInsets.symmetric(vertical: 20.0)),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/16740231/r/il/8b76ff/2084499904/il_1588xN.2084499904_8ywa.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/7659841/r/il/d34b8e/5236329344/il_794xN.5236329344_na4l.jpg",
+                      nomArticle: "Washable Sponge",
+                      price: "SGD 18.34",
+                      url: 'https://www.etsy.com/sg-en/listing/1417496225/eponge-lavable-un-accessoire-parfait'
+                    ),
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/16429726/r/il/c56a73/5616041924/il_1588xN.5616041924_bt70.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/19893040/r/il/63643f/4656506748/il_794xN.4656506748_1o1j.jpg",
+                      nomArticle: "Bamboo Toothbrushes",
+                      price: "SGD 7.16",
+                      url: 'https://www.etsy.com/sg-en/listing/961794247/brosses-a-dents-en-bambou-100-matieres'
+                    ),
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/40472291/r/il/83f44d/5428681650/il_1588xN.5428681650_tc8c.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/34097268/r/il/a53167/5684005381/il_794xN.5684005381_9gs1.jpg",
+                      nomArticle: "Reusable paper towels",
+                      price: "SGD 19.56",
+                      url: 'https://www.etsy.com/sg-en/listing/1634506066/serviettes-sans-papier-serviettes-en'
+                    ),
                   ],
                 ),
-                Padding(padding: EdgeInsets.symmetric(vertical: 30.0)),
+                Padding(padding: EdgeInsets.symmetric(vertical: 20.0)),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/16740231/r/il/8b76ff/2084499904/il_1588xN.2084499904_8ywa.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/23690462/r/il/3e1720/4817065201/il_794xN.4817065201_ab4s.jpg",
+                      nomArticle: "Natural Dish Brush Set",
+                      price: "SGD 50.43",
+                      url: 'https://www.etsy.com/sg-en/listing/1435661956/ensemble-de-brosses-a-vaisselle-zero'
+                    ),
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/16429726/r/il/c56a73/5616041924/il_1588xN.5616041924_bt70.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/45039598/r/il/c9688b/5677934855/il_794xN.5677934855_f5wh.jpg",
+                      nomArticle: "Tote Bag",
+                      price: "SGD 25.21",
+                      url: 'https://www.etsy.com/sg-en/listing/1647018963/sac-fourre-tout-pour-albums-esthetique'
+                    ),
                     PlanetSortMarketplaceItem(
-                        imagePath:
-                            "https://i.etsystatic.com/40472291/r/il/83f44d/5428681650/il_1588xN.5428681650_tc8c.jpg",
-                            nomArticle: "test2",
-                      price: "10€",),
+                      imagePath: "https://i.etsystatic.com/20877256/r/il/bac9b9/2202192583/il_794xN.2202192583_otxl.jpg",
+                      nomArticle: "Reusable organic cotton",
+                      price: "SGD 10.92",
+                      url: 'https://www.etsy.com/sg-en/listing/770869651/rondes-de-coton-bio-reutilisables-lune'
+                    ),
                   ],
                 ),
                 Padding(padding: EdgeInsets.symmetric(vertical: 45.0)),
-          ])
+              ]
+            )
           ],
         )
-              ,)
+      )
     );
   }
 }
