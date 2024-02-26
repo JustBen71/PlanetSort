e903d0f2d102278fb5552cc00944741a37ad2f8e
JustBen71500 <randazzobenjamin@outlook.fr>
2024-02-26 16:31:40 +0800

    DailyFile

--- lib/component/planetsort_dailytips.dart
+++ lib/component/planetsort_dailytips.dart
@@ -1,52 +1,114 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/widgets.dart';
 import 'package:planetsort/utils/constant.dart';
 
-class DailyTipsCard extends StatelessWidget {
-  final String tip;
-  //final VoidCallback onClose;
+class DailyTipWidget extends StatefulWidget {
+  const DailyTipWidget({Key? key}) : super(key: key);
 
-  const DailyTipsCard({
-    super.key,
-    required this.tip,
-    //required this.onClose,
-  });
+  @override
+  _DailyTipWidgetState createState() => _DailyTipWidgetState();
+}
+
+class _DailyTipWidgetState extends State<DailyTipWidget> {
+  bool _isVisible = true; // Initialisé à true pour que le widget soit visible au départ
+
+  void _toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible; // Bascule la visibilité
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    // Retourne un Container vide si _isVisible est false
+    if (!_isVisible) 
+    {
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            _isVisible = true;
+          });
+        },
+        child: const Card(
+        color: beige,
+        child: Padding(
+          padding:  EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+
+                children: [
+                  Text(
+                    'Daily Tips  ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "RockNRoll",
+                      color: green,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                  Icon(
+                    Icons.arrow_circle_down,
+                    color: green,
+                  ),
+                ]
+              )
+            ]
+          ),
+        )
+      )
+            );
+  }
+
     return Card(
-      color:
-          beige, // Couleur de fond de la carte, ajustez selon votre hexadécimal
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Expanded(
-              child: Text(
-                'Daily Tips',
-                style: TextStyle(
-                  fontWeight: FontWeight.normal,
-                  color: green, // Ajustez la couleur selon le besoin
+      /*shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(80),
+        topRight: Radius.circular(80),
+        bottomLeft: Radius.circular(20),
+        bottomRight: Radius.circular(20))
+      ),*/
+      color: beige,
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _isVisible = false;
+          });
+        },
+        child: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Daily Tips  ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: green,
+                    fontFamily: "RockNRoll",
+                  ),
                 ),
-              ),
-            ),
-            Expanded(
-              flex: 4,
-              child: Text(
-                tip,
-                style: const TextStyle(
-                  color: green, // Ajustez la couleur selon le besoin
+                Icon(
+                   Icons.arrow_circle_up, 
+                   color: green,
                 ),
-              ),
+              ],
             ),
-            IconButton(
-              icon:
-                  const Icon(Icons.close, color: green), // Couleur de l'icône X
-              onPressed: (() => {}),
+            Padding(padding: EdgeInsets.all(5.0)),
+            Text(
+              'Use public transportation or prefer environmentally friendly means of transportation.',
+              style: TextStyle(color: green, fontFamily: "RockNRoll", ),
+              textAlign: TextAlign.justify,
             ),
           ],
         ),
+      ),
       ),
     );
   }
